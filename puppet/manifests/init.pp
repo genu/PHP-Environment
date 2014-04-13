0e71458b7ac470df5f7b20b36efd8c37a7f59e4e
Eugen Istoc <eugenistoc@gmail.com>
2014-04-13 00:36:50 -0400

    add php, phing and xcdebug

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,3 +1,16 @@
-Exec {
-	path => ["/user/bin"]
+exec {'apt-get update':
+	command => '/usr/bin/apt-get update'
+
 }
+
+include apache
+include php
+include mysql
+
+php::pear::module { 'phing':
+  repository  => 'pear.phing.info',
+  alldeps => 'true',
+  use_package => 'no'
+}
+
+php::pecl::module { 'xdebug': }
