e7d9f180dbff7c4a566061b1c75403cd726a7ee0
Eugen Istoc <eugenistoc@gmail.com>
2014-04-13 01:06:51 -0400

    Fix provisioning of php extensions

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,16 +1,18 @@
-exec {'apt-get update':
-	command => '/usr/bin/apt-get update'
 
-}
 
 include apache
 include php
 include mysql
+
+class { 'apt':
+  force_aptget_update => true,
+}
 
 php::pear::module { 'phing':
   repository  => 'pear.phing.info',
   alldeps => 'true',
   use_package => 'no'
 }
+
 
 php::pecl::module { 'xdebug': }
