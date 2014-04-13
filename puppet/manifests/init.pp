81b4d3f13d166a09be8c12cb73b572483764cb0d
Eugen Istoc <eugenistoc@gmail.com>
2014-04-13 01:34:29 -0400

    Add git module

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -3,15 +3,20 @@
 include apache
 include php
 include mysql
+include git
 
 class { 'apt':
   force_aptget_update => true,
 }
 
 php::pear::module { 'phing':
-  repository  => 'pear.phing.info',
-  alldeps => 'true',
-  use_package => 'no'
+	repository  	=> 'pear.phing.info',
+	alldeps 		=> 'true',
+	use_package 	=> 'no'
+}
+
+php::pear::module { 'VersionControl_Git-0.4.4':
+	use_package	=> 'no'
 }
 
 
